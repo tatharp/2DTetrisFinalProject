aaac3f7eaf2cbcbf735ebe62dcb28b0820bc7983
tatharp <48963377+tatharp@users.noreply.github.com>
2019-05-01 22:49:56 -0500

    Piece randomizer and generator
    The changes made here add the ability to spawn a new random piece after an existing one hits the floor.

--- Game.cs
+++ Game.cs
@@ -11,13 +11,17 @@ public class Game : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+       SpawnNewPiece ();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+      public void SpawnNewPiece ()
+    {
+        GameObject nextPiece = (GameObject)Instantiate(Resources.Load(GetRandomPiece(), typeof(GameObject)), new Vector2(5.0f, 20.0f), Quaternion.identity);
     }
 
     public bool CheckIsInsideGrid (Vector2 pos)
@@ -29,4 +33,38 @@ public class Game : MonoBehaviour
     {
         return new Vector2 (Mathf.Round(pos.x), Mathf.Round(pos.y));
     }
+   string GetRandomPiece ()
+    {
+        int randomPiece = Random.Range(1, 8);
+
+        string randomPieceName = "Prefab/Tetromino_T";
+
+        switch (randomPiece)
+        {
+            case 1:
+                randomPieceName = "Prefab/Tetromino_T";
+                break;
+            case 2:
+                randomPieceName = "Prefab/Tetromino_Long";
+                break;
+            case 3:
+                randomPieceName = "Prefab/Tetromino_Square";
+                break;
+            case 4:
+                randomPieceName = "Prefab/Tetromino_J";
+                break;
+            case 5:
+                randomPieceName = "Prefab/Tetromino_L";
+                break;
+            case 6:
+                randomPieceName = "Prefab/Tetromino_S";
+                break;
+            case 7:
+                randomPieceName = "Prefab/Tetromino_Z";
+                break;
+        }
+
+        return randomPieceName;
+    }
 }
+
